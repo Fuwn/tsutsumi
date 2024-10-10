5159f5148267b3709afd9a4cb67223b7860f5612
Fuwn <contact@fuwn.me>
2024-10-09 21:58:16 -0700

    chore(flake.nxi): move inputs below outputs

--- flake.nix
+++ flake.nix
@@ -1,72 +1,6 @@
 {
   description = "Top-level Flake for Applications and Libraries Packaged for Nix";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    systems.url = "github:nix-systems/default";
-
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
-
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.systems.follows = "systems";
-    };
-
-    pre-commit-hooks = {
-      url = "github:cachix/git-hooks.nix";
-
-      inputs = {
-        flake-compat.follows = "flake-compat";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-
-    rui = {
-      url = "github:Fuwn/rui";
-
-      inputs = {
-        flake-compat.follows = "flake-compat";
-        flake-utils.follows = "flake-utils";
-        nixpkgs.follows = "nixpkgs";
-        pre-commit-hooks.follows = "pre-commit-hooks";
-        systems.follows = "systems";
-      };
-    };
-
-    gigi = {
-      url = "github:Fuwn/gigi";
-
-      inputs = {
-        flake-utils.follows = "flake-utils";
-        nixpkgs.follows = "nixpkgs";
-        systems.follows = "systems";
-      };
-    };
-
-    maple = {
-      url = "github:gemrest/maple";
-
-      inputs = {
-        flake-compat.follows = "flake-compat";
-        flake-utils.follows = "flake-utils";
-        nixpkgs.follows = "nixpkgs";
-        systems.follows = "systems";
-      };
-    };
-
-    mayu = {
-      url = "github:Fuwn/mayu";
-
-      inputs = {
-        flake-utils.follows = "flake-utils";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-  };
-
   outputs =
     {
       flake-utils,
@@ -152,4 +86,70 @@
         };
       }
     );
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    systems.url = "github:nix-systems/default";
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+
+      inputs = {
+        flake-compat.follows = "flake-compat";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    rui = {
+      url = "github:Fuwn/rui";
+
+      inputs = {
+        flake-compat.follows = "flake-compat";
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+        pre-commit-hooks.follows = "pre-commit-hooks";
+        systems.follows = "systems";
+      };
+    };
+
+    gigi = {
+      url = "github:Fuwn/gigi";
+
+      inputs = {
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+      };
+    };
+
+    maple = {
+      url = "github:gemrest/maple";
+
+      inputs = {
+        flake-compat.follows = "flake-compat";
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+      };
+    };
+
+    mayu = {
+      url = "github:Fuwn/mayu";
+
+      inputs = {
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
 }
