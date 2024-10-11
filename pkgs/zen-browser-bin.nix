8fa1728828157b808bab3e5b2d9c464eeb723a5e
Fuwn <contact@fuwn.me>
2024-10-11 00:43:29 -0700

    feat(pkgs): bump zen-browser-bin to 1.0.1-a.8

--- pkgs/zen-browser-bin.nix
+++ pkgs/zen-browser-bin.nix
@@ -4,7 +4,5 @@
   yae,
 }:
 import "${self}/lib/zen-browser-bin.nix" {
-  inherit (yae.zen-browser-bin) sha256;
-
-  version = "1.0.1-a.7";
+  inherit (yae.zen-browser-bin) sha256 version;
 } { inherit pkgs; }
