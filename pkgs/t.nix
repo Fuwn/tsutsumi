feb87b3e2dca96975cd5688a6d5e2658a2381ca1
Fuwn <contact@fuwn.me>
2024-10-14 18:53:46 -0700

    feat(pkgs): use upstream of t

--- pkgs/t.nix
+++ pkgs/t.nix
@@ -6,11 +6,11 @@ pkgs.buildRustPackage (
   with lib;
   {
     pname = "t";
-    version = "30a0bf6e5aec20c40e1b0c96fe9c6e3521997ff3";
-    githubOwner = "nixports";
+    version = "e3ed743";
+    githubOwner = "juev";
     githubHash = "sha256-4xlo+WecV0wAHaHRN37HvEaKnUHIRBSMk6BWn7PAIPc=";
-    cargoHash = "sha256-olRytgRRCj9V+V96Q1Eb+JOGxFKhhd/1C2Ge4NXv86Q=";
-    maintainers = with maintainers; [ Fuwn ];
+    cargoHash = "sha256-fz3cWgs8aqWkkOOItedWJE7OJnpO05+iSJYtj/yaqvc=";
+    maintainers = [ maintainers.Fuwn ];
     license = licenses.mit;
   }
 )
