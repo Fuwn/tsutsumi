ffa415d78db0702233dc35c4f8805e0938785819
Fuwn <contact@fuwn.me>
2024-12-25 13:36:39 -0800

    feat: package chibi-cli

---
+++ pkgs/chibi-cli/default.nix
@@ -0,0 +1,31 @@
+{
+  buildGo123Module,
+  fetchFromGitHub,
+  source,
+}:
+buildGo123Module rec {
+  pname = "chibi-cli";
+  version = source.version;
+  vendorHash = "sha256-nSEGkn6fhr0++9uzh/+7JHUwbj879fhs0zV2/i1AEYU=";
+  doCheck = false;
+
+  src = fetchFromGitHub {
+    owner = "CosmicPredator";
+    repo = pname;
+    rev = source.version;
+    hash = source.hash;
+  };
+
+  postPatch = ''
+    sed -i '3s/1.23.4/1.23.3/' go.mod
+    cp ${./go.sum} ./go.sum
+  '';
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-extldflags=-static"
+    "-X main.Version=${version}"
+    "-X main.Commit=${version}"
+  ];
+}
